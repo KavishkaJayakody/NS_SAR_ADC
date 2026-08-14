916ba4397b1290ec7dd73004ad6d35846f226fa2
KavishkaJayakody <jayakodkavishka@gmail.com>
2026-08-15 00:05:06 +0530

    layout of DEM CDAC and major layouts

--- libraries/NS_SAR_Digital/inverter/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++
@@ -1,1 +0,0 @@
-1777829347 /home/user22/NS_SAR_ADC/libraries/NS_SAR_Digital/inverter/test_inverter.v
--- libraries/NS_SAR_Digital/inverter/test_inverter.v
+++
@@ -1,3 +0,0 @@
-module test_inverter (input wire A, output wire B);
-  assign B = !A;
-endmodule
